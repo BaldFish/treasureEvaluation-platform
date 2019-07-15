4c65a1986747199a63fb76e3689c476ab2aee1af
lwc <wence.lv@cnlaunch.com>
2019-07-15 17:59:55 +0800

    更改baseURL

--- src/common/js/public.js
+++ src/common/js/public.js
@@ -1,8 +1,8 @@
-let baseURL="https://wallet-api-test.launchain.org";
+let baseURL="https://wallet-api-test.renrendao.net";
 let loginPlatform="https://login.datajs.com.cn";
 let exchangePlatform="https://exchange.datajs.com.cn";
 let transferPlatform="https://transfer.datajs.com.cn";
 let searchPlatform="https://search.datajs.com.cn";
-let bigDataURL="https://search-api-test.launchain.org";
+let bigDataURL="https://search-api-test.renrendao.net";
 export {baseURL,loginPlatform,exchangePlatform,transferPlatform,searchPlatform}
 
